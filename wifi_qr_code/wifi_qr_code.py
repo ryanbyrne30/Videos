00f275881098dbdf7585ead7456894a2f0ca691e
ryanbyrne30 <ryanjbyrne30@gmail.com>
2021-09-03 19:46:19 -0700

    Added windows to wifi_qr_code

--- wifi_qr_code/wifi_qr_code.py
+++ wifi_qr_code/wifi_qr_code.py
@@ -5,17 +5,48 @@ import os
 
 class GenQRCode(object):
     def __init__(self):
-        pass 
+        pass
 
     def run(self):
         if platform == 'linux' or platform == 'linux2':
             self.__linuxQRCode()
+        elif platform == 'win32':
+            self.__windowsQRCode()
         else:
             self.__defaultQRCode()
 
     def __linuxQRCode(self):
-        _, ssid, _, security, _, password = os.popen("nmcli device wifi show-password").read().split()
+        _, ssid, _, security, _, password = os.popen(
+            "nmcli device wifi show-password").read().split()
         self.__createQRCode(ssid, security, password)
+
+    def __windowsQRCode(self):
+        ssid = self.__windowsGetSSID()
+        security = self.__windowsGetSecurity(ssid)
+        password = self.__windowsGetPass(ssid)
+        self.__createQRCode(ssid, security, password)
+
+    def __windowsGetSSID(self):
+        output = os.popen("netsh wlan show interfaces").read().split()
+        for i in range(len(output)):
+            if output[i] == "SSID":
+                return output[i+2]
+
+    def __windowsGetSecurity(self, ssid):
+        output = os.popen(
+            f"netsh wlan show profile {ssid} key=clear").read().split()
+        for i in range(len(output)):
+            if output[i] == 'Authentication' and "WPA" in output[i+2]:
+                return "WPA"
+            if output[i] == 'Authentication' and "WEP" in output[i+2]:
+                return "WEP"
+
+    def __windowsGetPass(self, ssid):
+        output = os.popen(
+            f"netsh wlan show profile {ssid} key=clear").read().split()
+        for i in range(len(output)):
+            if output[i] == 'Key' and output[i+1] == 'Content':
+                return output[i+3]
 
     def __defaultQRCode(self):
         ssid = input("WiFi SSID (name): ")
@@ -25,7 +56,7 @@ class GenQRCode(object):
 
     def __createQRCode(self, ssid, security, password):
         qr.wifi_qrcode(ssid, False, security, password).save(f"{ssid}.png")
-        
+
 
 if __name__ == "__main__":
     GenQRCode().run()
