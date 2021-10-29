bb543dd1f1b142a8e48b9f4957e2f1022c2cea42
ryanbyrne30 <ryanjbyrne30@gmail.com>
2021-10-29 01:07:37 -0700

    Viterbi alg

---
+++ math/probstats/HMM_Viterbi.py
@@ -0,0 +1,82 @@
+import numpy as np
+from numpy.core.fromnumeric import argmax
+
+def HMM_Viterbi(seqA, seqB, seqR):
+    """If seqA implies seqB, find the most likely sequence
+    from factors in seqA that result in sequence seqR composed
+    from factors of seqB"""
+    factA = sorted(list(set(seqA)))
+    factB = sorted(list(set(seqB)))
+    factAMap = dict((factA[i], i) for i in range(len(factA)))
+    factBMap = dict((factB[i], i) for i in range(len(factB)))
+    tranAMat = np.zeros((len(factA), len(factA)))
+    tranBMat = np.zeros((len(factA), len(factB)))
+
+    counts = countAppearances(factAMap, seqA)
+
+    # compute transition matrix
+    for i in range(len(seqA)-1):
+        tranAMat[factAMap[seqA[i]]][factAMap[seqA[i+1]]] += 1
+    for i in range(len(seqB)):
+        tranBMat[factAMap[seqA[i]]][factBMap[seqB[i]]] += 1
+    tranAMat /= counts[:,None]
+    tranBMat /= counts[:,None]
+    return viterbi(factB, factA, counts/counts.sum(), seqR, tranAMat, tranBMat)
+
+
+    
+def countAppearances(factMap, seq):
+    count = np.zeros(len(factMap.keys()))
+    for s in seq:
+        count[factMap[s]] += 1
+    return count
+
+def viterbi(O, S, Pi, Y, A, B):
+    """
+    O - observation space [ o1, o2, ..., oN ]
+    S - state space [ s1, s2, ..., sK ]
+    Pi - array of initial probabilities [ pi1, pi2, ..., piK ]
+    Y - sequence of observations [ y1, y2, ..., yT ]
+    A - transition matrix of states of size K x K
+    B - emission matrix of states to observations of size K x N
+    """
+    k = len(S)
+    t = len(Y)
+
+    T1 = np.zeros((k, t))
+    T2 = np.zeros((k, t))
+    T1[:,0] = B[:,O.index(Y[0])] * Pi
+    for j in range(1, t):
+        for i in range(k):
+            o = O.index(Y[j])
+            c = T1[:,j-1] * A[:,i] * B[i, o]
+            T1[i, j] = np.max(c)
+            T2[i, j] = argmax(c)
+
+    print("T1")
+    printMatrix(T1)
+    print("T2")
+    printMatrix(T2)
+
+    z = argmax(T1[:,-1])
+    print(T1[:,-1])
+    print("Z", z)
+    x = [ S[z] ]
+    for j in range(t-1, 0, -1):
+        z = int(T2[z, j])
+        x = [ S[z] ] + x
+    return x, max(T1[:, -1])
+
+def printMatrix(M):
+    for i in range(M.shape[0]):
+        print(" ".join([ "%.3f" %v for v in M[i] ]))
+
+
+if __name__ == "__main__":
+    seqA = list("cabbaaabbbddaa")
+    seqB = list("22121100001221")
+    seqR = list("12101")
+
+    x, p = HMM_Viterbi(seqA, seqB, seqR)    
+    print(x)
+    print(f"{round(p*100, 2)}%")
