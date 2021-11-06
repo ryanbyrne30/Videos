9c51e31311211dc9b73fb5bfdf9faaab20a1b1c7
ryanbyrne30 <ryanjbyrne30@gmail.com>
2021-11-06 01:55:06 -0700

    Fourier 2 part getting there

--- math/Fourier/simulations/DFT/display.py
+++ math/Fourier/simulations/DFT/display.py
@@ -1,15 +1,44 @@
 import cmath
 import pygame
+import numpy as np
 
-def drawEpicenter(screen, center, radius, freq, t):
+def drawEpicenter(screen, center, radius, freq, phase, t):
     surface = pygame.Surface((radius*2, radius*2), pygame.SRCALPHA)
     circ_center = radius, radius
     pygame.draw.circle(surface, (255,255,255, 15), circ_center, radius, 0)
-    
-    c = cmath.rect(radius, t*freq)
+    c = cmath.rect(radius, (t*freq)+phase)
     point = c.real+radius, -c.imag+radius 
     pygame.draw.line(surface, (255, 255, 255, 100), circ_center, point)
 
     screen.blit(surface, (center[0]-radius, center[1]-radius))
 
     return point[0]+center[0]-radius, point[1]+center[1]-radius
+
+def dft(cpoints):
+    X = []
+    N = len(cpoints)
+    for k in range(N):
+        sum = 0
+        for n in range(N):
+            sum += cpoints[n] * cmath.exp(-2j*cmath.pi*k*n/N)
+
+        avg = sum / N
+        freq = k 
+        amp = np.linalg.norm(avg)
+        phase = np.arctan(avg.imag/avg.real)
+
+        X.append([avg, freq, amp, phase])
+    return X
+
+# def drawEpicenter(screen, center, freq, amp, phase, t):
+#     surface = pygame.Surface((radius*2, radius*2), pygame.SRCALPHA)
+#     circ_center = radius, radius
+#     pygame.draw.circle(surface, (255,255,255, 15), circ_center, radius, 0)
+    
+#     c = cmath.rect(radius, t*freq)
+#     point = c.real+radius, -c.imag+radius 
+#     pygame.draw.line(surface, (255, 255, 255, 100), circ_center, point)
+
+#     screen.blit(surface, (center[0]-radius, center[1]-radius))
+
+#     return point[0]+center[0]-radius, point[1]+center[1]-radius
--- math/Fourier/simulations/DFT/main.py
+++ math/Fourier/simulations/DFT/main.py
@@ -2,14 +2,17 @@ import pygame
 import numpy as np
 import cmath 
 from display import *
+from random import randrange
 
 WIDTH, HEIGHT = 1800, 1000
 PI = cmath.pi
+SCALE = 250
 
-SINE_FREQS = [ 1, 3, 5, 7 ]
-COS_FREQS = [ 2, 2, 3, 4 ]
-SINE_RADS = np.array([ 4/PI, 4/(3*PI), 4/(5*PI), 4/(7*PI) ]) * 30
-COS_RADS = np.array([ 4/PI, 4/(3*PI), 4/(5*PI), 4/(7*PI) ]) * 30
+# POINTS = [ randrange(0, SCALE)+randrange(0, SCALE)*1j for _ in range(4) ]
+POINTS = [ i+i*1j for i in range(0, 150, 10) ]
+FOURIER = dft(POINTS)
+
+print(FOURIER)
 
 # colors
 BLACK = 0,0,0
@@ -25,6 +28,7 @@ path = []
 t = 0
 clock = pygame.time.Clock()
 running = True
+isPaused = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -33,21 +37,32 @@ while running:
             isPaused = True if not isPaused else False 
     screen.fill(BLACK)
 
-    circSine_center = drawEpicenter(screen, (WIDTH/8, HEIGHT/2), SINE_RADS[0], SINE_FREQS[0], t)
-    circCos_center = drawEpicenter(screen, (WIDTH/2, HEIGHT/8), COS_RADS[0], COS_FREQS[0], t)
-    for i in range(1, len(SINE_FREQS)):
-        circCos_center = drawEpicenter(screen, circCos_center, SINE_RADS[i], SINE_FREQS[i], t)
-        circSine_center = drawEpicenter(screen, circSine_center, SINE_RADS[i], SINE_FREQS[i], t)
+    for p in POINTS:
+        pygame.draw.circle(screen, (255,0,0), (WIDTH/2+p.real, HEIGHT/2-p.imag), 4)
+        pygame.draw.circle(screen, (255,0,0), (WIDTH/8, HEIGHT/2-p.imag), 4)
+        pygame.draw.circle(screen, (255,0,0), (WIDTH/2+p.real, HEIGHT/8), 4)
+
+    # circCos_center = drawEpicenter(screen, (WIDTH/2, HEIGHT/8), FOURIER[0][2], FOURIER[0][1], FOURIER[0][3], t)
+    circSine_center = drawEpicenter(screen, (WIDTH/8, HEIGHT/2), FOURIER[0][2], FOURIER[0][1], FOURIER[0][3], t)
+    for avg, freq, amp, phase in FOURIER[1:]:
+        # circCos_center = drawEpicenter(screen, circCos_center, amp, freq, phase, t)
+        circSine_center = drawEpicenter(screen, circSine_center, amp, freq, phase, t)
 
     pathStart = WIDTH/2, HEIGHT/2
-    point = circCos_center[0], circSine_center[1]
+    # point = circCos_center[0], circSine_center[1]
+    point = WIDTH/2, circSine_center[1]
     path.insert(0, point)
-    for x, y in path:
-        pygame.draw.circle(screen, (100, 100, 20), (x, y), 2)
+    for i in range(len(path)):
+    # for x, y in path:
+        pygame.draw.circle(screen, (100, 100, 20), (WIDTH/2+i, path[i][1]), 2)
+        # pygame.draw.circle(screen, (100, 100, 20), x, y), 2)
     pygame.draw.line(screen, (255,255,255), circSine_center, point)
-    pygame.draw.line(screen, (255,255,255), circCos_center, point)
+    # pygame.draw.line(screen, (255,255,255), circCos_center, point)
 
     path = path[:int(WIDTH/2)]
+
+    # dt = cmath.pi*2/len(FOURIER)
+    # t += dt / 2
     t += 0.01
     pygame.display.update()
     clock.tick(100)
